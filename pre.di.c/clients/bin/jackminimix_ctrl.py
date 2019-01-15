585b05e43aa716ebd44eb1c6bb2b53d2548d6dcb
Rsantct <rsantct@gmail.com>
2019-01-15 22:19:56 +0100

    Create jackminimix_ctrl.py

---
+++ pre.di.c/clients/bin/jackminimix_ctrl.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import argparse
+from subprocess import run, check_output
+import sys
+
+def get_args():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--ip', default='localhost',
+        help='The ip of the OSC server')
+
+    parser.add_argument('--port', type=int, default=9995,
+        help='The port the OSC server is listening on')
+
+    # The first option '--input' is the attribute name when refering to it, 
+    # next options will be treated as alias.
+    parser.add_argument('--input', '--i', '-i', type=int, default=0,
+        help='The mixer channel 1-4')
+
+    parser.add_argument('--gain', '--g', '-g', type=int, default=0,
+        help='The channel gain -99...+99 (dB)')
+
+    return parser.parse_args()
+
+def get_port():
+    port=9995
+    try:
+        lines = check_output( 'pgrep -fa jackminimix'.split() ).decode()
+        for line in lines.split('\n'):
+            if 'minimix -p' in line:
+                port = int( line.split('-p')[1].strip().split()[0] )
+    except:
+        print('jackminimix process not found')
+        sys.exit(-1)
+    return port
+    
+if __name__ == '__main__':
+    
+    args = get_args()
+    
+    cmd = f'oscchief send { args.ip } { str(get_port()) } \
+           /mixer/channel/set_gain ii { args.input } { args.gain }'
+
+    #print(cmd)
+    run( cmd.split() )
+
