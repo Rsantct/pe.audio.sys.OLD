40978b609ab875fe922ff52a07db2fc4345a1252
Rsantct <rsantct@gmail.com>
2019-02-06 17:02:14 +0100

    Update syseq.py

--- pre.di.c/clients/bin/syseq.py
+++ pre.di.c/clients/bin/syseq.py
@@ -3,26 +3,23 @@
     pre.di.c. sysEQ generator curves ported from FIRtro.
 
     Calculates the target curve of the loudspeaker system.
-    
-    WORK IN PROGRESS
 """
 
 import sys
 import numpy as np
+from signal.scipy import hilbert
 import yaml
 import basepaths as bp
 import getconfigs as gc
 import curves
 
-# SYSEQ magnitude curve path (!) work in progress
-SYSEQ_MAG_PATH = 'SYSEQ.dat' # R20_ext-target_mag.dat
-
 if __name__ == '__main__':
     
     # Read target parameteres inside loudspeaker definition file
     try:
-        lspk_path = sys.argv[1]
-        lspk_path += f'/{lspk_path.split("/")[-1]}.yml'
+        lspk_path   = sys.argv[1]
+        lspk_name = f'{lspk_path.split("/")[-1]}'
+        lspk_path  += f'/{lspk_name}.yml'
         with open( lspk_path , 'r' ) as f:
             lspk_config = yaml.load( f.read() )
         room_gain    = lspk_config['room_gain']   
@@ -32,19 +29,26 @@ if __name__ == '__main__':
         print( 'Error reading loudspeaker definition file' )
         sys.exit()
 
-    # Prepare target curve
-    freq  = np.loadtxt( f'{bp.config_folder}/{gc.config["frequencies"]}' )
-    curve = np.zeros(len(freq))
+    # Filenames
+    syseq_mag_path = f'{lspk_path.replace(".yml", "_target_mag.dat")}.candidate'
+    syseq_pha_path = f'{lspk_path.replace(".yml", "_target_mag.dat")}.candidate'
 
-    #if system_eq   # Former option from FIRtro, but on
-    if True:        # pre.di.c target will be always applied
-        if house_atten > 0:
-            house = curves.HouseCurve( freq, house_corner, house_atten )
-        else:
-            house = np.zeros( len(freq) )
-        room = curves.RoomGain( freq, room_gain )
-        curve = curve + house + room
+    # Prepare target curve
+    freq   = np.loadtxt( f'{bp.config_folder}/{gc.config["frequencies"]}' )
+    eq_mag = np.zeros(len(freq))
+
+    if house_atten > 0:
+        house = curves.HouseCurve( freq, house_corner, house_atten )
+    else:
+        house = np.zeros( len(freq) )
+    room = curves.RoomGain( freq, room_gain )
+
+    # Compose magnitudes
+    eq_mag = eq_mag + house + room
+    # Derive the phase
+    eq_pha = np.conj( hilbert( np.abs(eq_mag) ) )
 
     # Write data to file
-    np.savetxt (SYSEQ_MAG_PATH, curve)
-    print( f'Curve stored at: {SYSEQ_MAG_PATH}' )
+    np.savetxt (syseq_mag_path, eq_mag)
+    np.savetxt (syseq_pha_path, eq_pha)
+    print( f'Target curves stored at:\n{syseq_mag_path}\n{syseq_pha_path}' )
