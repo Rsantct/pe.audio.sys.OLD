729eef17ff59a924542c4e8a35af0c0c423b338f
Rsantct <rsantct@gmail.com>
2019-02-10 13:17:35 +0100

    Update download_predic.sh

--- .install/download_predic.sh
+++ .install/download_predic.sh
@@ -2,7 +2,6 @@
 
 ####################################################
 # CONFIGURE HERE the REPOSITORY to download from
-#reposite='https://github.com/rripio'
 reposite='https://github.com/rsantct'
 ####################################################
 
@@ -10,14 +9,14 @@ echo
 echo "WARNING: Will download from: [ ""$reposite"" ]"
 read -r -p "         Is this OK? [y/N] " tmp
 if [ "$tmp" != "y" ] && [ "$tmp" != "Y" ]; then
-    echo '(i) Remember to edit tmp/download_predic.sh to point to the desired repo.'
+    echo '(i) Remember to edit tmp/download_peaudiosys.sh to point to the desired repo.'
     echo '    Bye.'
     exit 0
 fi
 
 if [ -z $1 ] ; then
     echo usage:
-    echo "    download_predic.sh  master | another_branch"
+    echo "    download_peaudiosys.sh  master | another_branch"
     echo
     exit 0
 fi
@@ -29,10 +28,10 @@ cd $HOME/tmp
 
 # Removes any existent master.zip or predic directory for this branch:
 rm $branch.zip
-rm -r pre.di.c-$branch
+rm -r pe.audio.sys-$branch
 
 # Downloads the zip
-wget "$reposite"/pre.di.c/archive/"$branch".zip
+wget "$reposite"/pe.audio.sys/archive/"$branch".zip
 
 # Unzip
 unzip $branch.zip
@@ -40,8 +39,8 @@ rm $branch.zip
 
 ######################## (i) ######################## 
 # Drops the installing (download and update) scripts into tmp/ to be accesible
-cp -f pre.di.c-$branch/.install/*sh .
+cp -f pe.audio.sys-$branch/.install/*sh .
 
 # And back to home
 cd
-rm download_predic.sh > /dev/null 2>&1
+rm download_peaudiosys.sh > /dev/null 2>&1
