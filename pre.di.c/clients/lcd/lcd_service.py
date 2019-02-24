c4d70196628183b6a50e19acad33b282f876b9dd
Rsantct <rsantct@gmail.com>
2019-02-24 16:42:25 +0100

    adding loudness_ref and monitor values

--- pre.di.c/clients/lcd/lcd_service.py
+++ pre.di.c/clients/lcd/lcd_service.py
@@ -45,17 +45,25 @@ import lcd_client
 import players
 
 LEVEL_OLD = None
-# Will watch for files changed on this folder and subfolders:
-WATCHED_DIR =       f'{HOME}/pre.di.c/'
-# The files we are going to pay attention to:
-STATUS_file     = f'{HOME}/pre.di.c/config/state.yml'
-LIBRESPOT_file  = f'{HOME}/pre.di.c/.librespot_events'
-SPOTIFY_file    = f'{HOME}/pre.di.c/.librespot_events'
-ISTREAMS_file   = f'{HOME}/pre.di.c/.istreams_events'
-DVB_file        = f'{HOME}/pre.di.c/.dvb_events'
-MPD_file        = f'{HOME}/pre.di.c/.mpd_events'
 
-# Read LCD settings
+# 'loudness_track' as global because loudness_monitor value does not
+# belong to the pre.di.c status dict and the updater needs to kwow about it.
+loudness_track = False
+# An auxiliary counter to divide the high refresh rate of LOUDNESSMON_file
+lmc = 0
+
+# Will watch for files changed on this folder and subfolders:
+WATCHED_DIR      = f'{HOME}/pre.di.c/'
+# The files we are going to pay attention to:
+STATUS_file      = f'{HOME}/pre.di.c/config/state.yml'
+LIBRESPOT_file   = f'{HOME}/pre.di.c/.librespot_events'
+SPOTIFY_file     = f'{HOME}/pre.di.c/.librespot_events'
+ISTREAMS_file    = f'{HOME}/pre.di.c/.istreams_events'
+DVB_file         = f'{HOME}/pre.di.c/.dvb_events'
+MPD_file         = f'{HOME}/pre.di.c/.mpd_events'
+LOUDNESSMON_file = f'{HOME}/pre.di.c/.loudness_monitor'
+
+# Reading the LCD SETTINGS:
 f = open( f'{bp.main_folder}/clients/lcd/lcd.yml', 'r' )
 tmp = f.read()
 f.close()
@@ -96,34 +104,16 @@ def show_temporary_screen( message, timeout=LCD_CONFIG['info_screen_timeout'] ):
         if line > 4:
             break
 
-def prepare_main_screen():
-    """ Defines pre.di.c info main screen 'src_1' and his set of widgets """
-    # Adding the screen itself:
-    LCD.send('screen_add scr_1')
-
-    # Definig the set of widgets (widgets_xxxx becomes global variables)
-    define_widgets()
-
-    # Adding the widgets to the main screen
-    # pre.di.c status widgets
-    for wName, wProps in widgets_state.items():
-        cmd = f'widget_add scr_1 { wName } string'
-        LCD.send( cmd )
-    # metadata players widgets
-    for wName, wProps in widgets_meta.items():
-        cmd = f'widget_add scr_1 { wName } scroller'
-        LCD.send( cmd )
-
 def define_widgets():
 
     # The screen layout draft:
     #       0        1         2
     #       12345678901234567890
     #
-    # 1     Vol: -15.0   Bal: -1
-    # 2     B:-1 T:-2  LOUD MONO
-    # 3     Input: inputname
-    # 4     metadata_title_marquee
+    # 1     v:-15.0  bl:-1  MONO
+    # 2     b:+1 t:-2 LOUDref 12
+    # 3     inputname    lmon 12
+    # 4     a_metadata_marquee_
 
     # The widget collection definition (as global variables)
     # Values are defaults, later update_status() will add the current 
@@ -133,16 +123,16 @@ def define_widgets():
     
     global widgets_state # defined text type when prepare_main_screen
     widgets_state = {
-                'input'             : { 'pos':'1  3',    'val':'input:'   },
-                'level'             : { 'pos':'1  1',    'val':'vol:'     },
+                'input'             : { 'pos':'1  3',    'val':''         },
+                'level'             : { 'pos':'1  1',    'val':'v:'       },
                 'headroom'          : { 'pos':'0  0',    'val':'hrm:'     },
-                'balance'           : { 'pos':'14 1',    'val':'bal:'     },
-                'mono'              : { 'pos':'12 2',    'val':'MONO'     },
+                'balance'           : { 'pos':'10 1',    'val':'bl:'      },
+                'mono'              : { 'pos':'17 1',    'val':'MONO'     },
                 'muted'             : { 'pos':'1  1',    'val':'MUTED    '},
                 'bass'              : { 'pos':'1  2',    'val':'b:'       },
                 'treble'            : { 'pos':'6  2',    'val':'t:'       },
-                'loudness_ref'      : { 'pos':'0  0',    'val':''         },
-                'loudness_track'    : { 'pos':'17 2',    'val':'LOUD'     },
+                'loudness_ref'      : { 'pos':'15 2',    'val':'ref'      },
+                'loudness_track'    : { 'pos':'11 2',    'val':'LOUD'     },
                 'XO_set'            : { 'pos':'0  0',    'val':'xo:'      },
                 'DRC_set'           : { 'pos':'0  0',    'val':'drc:'     },
                 'PEQ_set'           : { 'pos':'0  0',    'val':'peq:'     },
@@ -150,6 +140,11 @@ def define_widgets():
                 'polarity'          : { 'pos':'0  0',    'val':'pol'      }
                 }
 
+    global widgets_aux # info outside pre.di.c status
+    widgets_aux = {
+                'loudness_monitor'  : { 'pos':'15 3',    'val':'mon'     }
+                }
+                
     global widgets_meta # defined scroller type when prepare_main_screen
     widgets_meta = {
                 'artist'            : { 'pos':'0  0',    'val':'' },
@@ -158,24 +153,62 @@ def define_widgets():
                 'bottom_marquee'    : { 'pos':'1  4',    'val':'' },
                 }
 
+def prepare_main_screen():
+    """ Defines pre.di.c info main screen 'src_1' and his set of widgets """
+    # Adding the screen itself:
+    LCD.send('screen_add scr_1')
+
+    # Definig the set of widgets (widgets_xxxx becomes global variables)
+    define_widgets()
+
+    # Adding the previously defined widgets to the main screen:
+    
+    # 1) pre.di.c status widgets
+    for wName, wProps in widgets_state.items():
+        cmd = f'widget_add scr_1 { wName } string'
+        LCD.send( cmd )
+    # 2) Aux widgets
+    for wName, wProps in widgets_aux.items():
+        cmd = f'widget_add scr_1 { wName } string'
+        LCD.send( cmd )
+    # 3) metadata players widgets
+    for wName, wProps in widgets_meta.items():
+        cmd = f'widget_add scr_1 { wName } scroller'
+        LCD.send( cmd )
+
 def update_status():
     """ Reads pre.di.c/config/state.yml then updates the LCD """
     # http://lcdproc.sourceforge.net/docs/lcdproc-0-5-5-user.html
 
     def show_status(data, priority="info"):
         global LEVEL_OLD
+        global loudness_track
         
         for key, value in data.items():
-            pos = widgets_state[key]['pos']
-            lab = widgets_state[key]['val']
+            pos = widgets_state[key]['pos'] # pos ~> position
+            lab = widgets_state[key]['val'] # lab ~> label
 
-            # If boolean value, will keep the defalul widget value or will supress it
+            # When booleans (loudness_track, muted, mono)
+            # will leave the defalul widget value or will supress it
             if type(value) == bool:
                 if not value:
                     lab = ''
+                # Update global to be accesible outside from auxiliary
+                if key == 'loudness_track':
+                    loudness_track = value
+                    
+            # Special case: loudness_ref will be rounded to integer
+            elif key == 'loudness_ref':
+                if data['loudness_track']:
+                    lab += str( int(round(value,0)) ).rjust(3)
+                else:
+                    lab = ''
+                    
+            # Any else key:
             else:
                 lab += str(value)
 
+                
             # sintax for string widgets:
             #   widget_set screen widget coordinate "text"
             cmd = f'widget_set scr_1 { key } { pos } "{ lab }"'
@@ -188,9 +221,37 @@ def update_status():
 
     with open(STATUS_file, 'r') as f:
         show_status( yaml.load( f.read() ) )
+        
+def update_loudness_monitor():
+    """ Reads the monitored value inside the file .loudness_monitor
+        then updates the LCD display.
+    """
+    
+    def show_loudness_monitor(value):
+        wdg = 'loudness_monitor'
+        pos = widgets_aux[wdg]['pos']
+        lab = widgets_aux[wdg]['val']
+        
+        value = int( round(value,0) )
+        if loudness_track:
+            lab += str(value).rjust(3)
+        else:
+            lab = ''
+            
+        cmd = f'widget_set scr_1 { wdg } { pos } "{ lab }"'
+        #print(cmd)
+        LCD.send( cmd )
+    
+    with open(LOUDNESSMON_file, 'r') as f:
+        value = f.read().strip()
+        try:
+            value = round( float(value), 1)
+        except:
+            value = 0.0
+        show_loudness_monitor( value )
 
 def update_metadata(metadata, mode='composed_marquee', scr='scr_1'):
-    """ Reads pre.di.c metadata dict then updates the LCD """
+    """ Reads pre.di.c metadata dict then updates the LCD display """
     # http://lcdproc.sourceforge.net/docs/lcdproc-0-5-5-user.html
 
     def compose_marquee(md):
@@ -244,14 +305,12 @@ class changed_files_handler(FileSystemEventHandler):
 
         path = event.src_path
 
-        # pre.di.c state has changed
-        if path == STATUS_file:
-            #print('updating status')
+        # The pre.di.c state has changed
+        if STATUS_file in path:
             update_status()
 
-        # a player events file has changed
-        if path in (STATUS_file,
-                    MPD_file,
+        # A player event file has changed
+        if path in (MPD_file,
                     SPOTIFY_file,
                     LIBRESPOT_file,
                     DVB_file,
@@ -259,6 +318,15 @@ class changed_files_handler(FileSystemEventHandler):
             sleep(1) # avoids bouncing
             # needs decode() because players gives bytes-like
             update_metadata( players.player_get_meta().decode() , mode='composed_marquee')
+
+        # The loudness monitor file has changed
+        # loudness monitor changes counter
+        global lmc
+        if path in (LOUDNESSMON_file):
+            lmc += 1        # the auxiliary loudness monitor refresh counter
+            if lmc > 10:    # waits for 10 because file update period is 100 ms
+                update_loudness_monitor()
+                lmc = 0
 
 if __name__ == "__main__":
 
@@ -277,6 +345,8 @@ if __name__ == "__main__":
 
     # Displays the state of pre.di.c
     update_status()
+    # Displays update_loudness_monitor
+    update_loudness_monitor()
     
     # Displays metadata
     #md =  '{"artist":"Some ARTIST",'
