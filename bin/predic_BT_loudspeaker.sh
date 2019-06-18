8a14c346dc0936079c6b8cb6bf1862bcfaf77da9
Rsantct <rsantct@gmail.com>
2019-06-18 12:31:38 +0200

    Update predic_BT_loudspeaker.sh

--- bin/predic_BT_loudspeaker.sh
+++ bin/predic_BT_loudspeaker.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 # This script starts the necessary jack to PA ports and the PA loopack,
-# in order to use a BT loudspeaker under PA as a predic input follower
+# in order to use a BT loudspeaker under PA as a predic input monitor.
 
 # It is needed that BT loudspeaker is paired and available under PA.
 
 # Remember you need to declare the jack to PA ports 'pulse_source' inside 
 # the 'jack_monitors:' section under <config.yml>
 
+# (i) Configure here the BT loudspeaker address or give it as argument
+BTaddr=00:0C:8A:E1:F8:89
+
+if [ $1 ]; then
+  BTaddr=$1
+fi
 
 # Stopping 
 ~/pre.di.c/init/pulseaudio-jack-source+loopback stop
 sleep 1
 
 # Starting
-~/pre.di.c/init/pulseaudio-jack-source+loopback start &
+~/pre.di.c/init/pulseaudio-jack-source+loopback start $BTaddr &
 sleep 3
 
 # Reconnecting the current input in order to reach the new monitor ports
