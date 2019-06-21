db0ece97e094d52498a4fec5b79c44cf65f69723
Rsantct <rsantct@gmail.com>
2019-06-21 23:06:07 +0200

    Create PA_BT_1st_device.py

---
+++ pre.di.c/clients/PA_BT_lspk_monitor/PA_BT_1st_device.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+""" Simply prints the BT asddress of the
+    1st BT loudspeaker as listed in Pulseaudio
+"""
+
+import sys
+import subprocess as sp
+
+def BTdescription(address):
+    """ Gets the device.description from the PA card of given address xx:xx:...
+    """
+    address = address.replace(':', '_')
+    BTdescription = ''
+
+    tmp = sp.check_output( 'pactl list cards'.split() ).decode()
+
+    found = False
+    for line in tmp.split('\n'):
+        if address in line:
+            found = True
+        if ('device.description' in line) and found:
+            BTdescription = line.split('=')[-1].replace('"', '').strip()
+    return BTdescription
+
+def find_1st_BT_device():
+    """ Finds the 1st BT loudspeaker as listed in Pulseaudio.
+        Returns it as a card dictionary
+    """
+    cards = []
+    tmp = sp.check_output( 'pactl list cards short'.split() ).decode()
+    for line in tmp.split('\n'):
+        if not line:
+            break
+        #print (line)
+        cards.append( {'num': line.split()[0], 'name': line.split()[1]} )
+    for card in cards:
+        if 'bluez_card' in card['name']:
+            card['string'] = card['name'].split('.')[-1].replace('_',':')
+            card['description'] = BTdescription( card['string'] )
+            return card
+    return {'string': ''}
+
+print( find_1st_BT_device()['string'] )
+
+
